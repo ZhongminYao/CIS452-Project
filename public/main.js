08fedc14f7e8c23f8f2bd7f99d29b4f80c2a3094
Tyson Owens <tysoneowens@gmail.com>
2019-02-13 22:49:02 -0500

    trying external link

--- public/main.js
+++ public/main.js
@@ -27,7 +27,7 @@ function isUserSignedIn() {
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 function authStateObserver(user) {
   if (user) {
-    window.location.replace("cis-454.firebaseapp.com/home.html");
+    window.location.replace('https://cis-454.firebaseapp.com/home.html');
   }
 }
 // Shortcuts to DOM Elements.
