dd7aa1ce10da81def04c2b45c325f031d6f75b45
Leo0o0o0o <45006225+Leo0o0o0o@users.noreply.github.com>
2019-02-13 20:11:05 -0500

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,38 +1,46 @@
-	
+
 'use strict';
 
-	var signInElement = document.getElementById('signInWithG');
-
-	var logoElement = document.getElementById('logo');
-
-      signInElement.addEventListener('click', signIn);
-
-     function signIn() {
-    // Sign into Firebase using popup auth & Google as the identity provider.
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-    
-    }
-
-    function initFirebaseAuth() {
- 	 // Listen to auth state changes.
-  	firebase.auth().onAuthStateChanged(authStateObserver);
-	}
-
-	function authStateObserver(user) {
-  	if (user) { 
-  		logoElement.removeAttribute('hidden');
-  }
+// Signs-in Friendly Chat.
+function signIn() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 }
 
-    function isUserSignedIn() {
-      return !!firebase.auth().currentUser;
-    }
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
 
-	checkSetup();
+// Initiate firebase auth.
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
 
-	var firestore = firebase.firestore();
-	var settings = {timestampsInSnapshots: true};
-	firestore.settings(settings);
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
 
-    initFirebaseAuth();
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) { 
+    window.location.replace("http://www.google.com");
+  } 
+}
+// Shortcuts to DOM Elements.
+
+var signInButtonElement = document.getElementById('signInWithG');
+var logoElement = document.getElementById('logo');
+
+signInButtonElement.addEventListener('click', signIn);
+
+// initialize Firebase
+initFirebaseAuth();
+
+// Remove the warning about timstamps change. 
+var firestore = firebase.firestore();
+var settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
