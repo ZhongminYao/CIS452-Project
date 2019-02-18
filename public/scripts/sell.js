221712d54c44d75198f9f72c52f8fbfffced65fe
Leo0o0o0o <45006225+Leo0o0o0o@users.noreply.github.com>
2019-02-18 11:56:46 -0500

    Add files via upload

---
+++ public/scripts/sell.js
@@ -0,0 +1,41 @@
+var dbElement = document.getElementById('submit');
+dbElement.addEventListener('click', reload);
+
+var TBElement = document.getElementById('TBname');
+var AElement = document.getElementById('Aname');
+var PElement = document.getElementById('price');
+
+var config = {
+                  apiKey: "AIzaSyAmEbdMxefi-2wt7u8_hrFHLixM_RCLmwE",
+                  authDomain: "cis-454.firebaseapp.com",
+                  databaseURL: "https://cis-454.firebaseio.com",
+                  projectId: "cis-454",
+                  storageBucket: "cis-454.appspot.com",
+                  messagingSenderId: "737490005392"
+                };
+
+                firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+function reload(){
+  if (TBElement.value && AElement.value && PElement.value) {
+    upload().then(function(){
+      window,location.reload();
+   });
+    window.alert('You have successfully add this textbook to store!');
+  }
+  else window,location.reload();
+}
+
+
+function upload() {
+  // Add a new message entry to the Firebase database.
+    return db.collection('Textbooks').add({
+      Title: TBElement.value,
+      Author: AElement.value,
+      Price: PElement.value
+   }).catch(function(error) {
+     console.error('Error writing new message to Firebase Database', error);
+   });
+}
