f866a87f33caf2a404f17eee88bda0b36bb48ea1
Tyson Owens <tysoneowens@gmail.com>
2019-02-19 21:55:16 -0500

    added extra checks for payment page

--- public/payment.js
+++ public/payment.js
@@ -20,8 +20,16 @@ var Year = Number(document.getElementById('expyear').value);
 var Code = document.getElementById('cvv');
 var Card = document.getElementById('fname');
 var CYear = new Date().getFullYear();
+var Address = document.getElementById('adr');
+var City = document.getElementById('city');
+var State = document.getElementById('state');
+var Zip = document.getElementById('zip');
+var LName = document.getElementById('lname');
+var Month = document.getElementById('expmonth');
+
 var regex = /[0-9]{16}/;
 var regex2 = /[0-9]{3}/;
+var regex3 = /[0-9]{5}/;
 var bookKey = window.location.hash.substr(1);
 
 function authPayment(){
@@ -31,7 +39,15 @@ function authPayment(){
   var Code = document.getElementById('cvv');
   var Card = document.getElementById('fname');
   var CYear = new Date().getFullYear();
-  if (Card.value && Name.value && (CNumber.value).match(regex) && (Code.value).match(regex2)) {
+  //
+  var Address = document.getElementById('adr');
+  var City = document.getElementById('city');
+  var State = document.getElementById('state');
+  var Zip = document.getElementById('zip');
+  var LName = document.getElementById('lname');
+  var Month = document.getElementById('expmonth');
+
+  if (Card.value && Name.value && Address.value && City.value && State.value && LName.value && Month.value && (CNumber.value).match(regex) && (Code.value).match(regex2) && (Zip.value).match(regex3)) {
     if(CYear <= Year){
       db.collection('Textbooks').doc(bookKey).update({
         Status: 'Sold'
@@ -41,6 +57,6 @@ function authPayment(){
       window.alert('Expired Date');
     }
   } else {
-    window.alert('Invalid Card');
+    window.alert('Invalid Information');
   }
 }
